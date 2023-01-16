5648cdd0fe44e1f208e83c5a8aab14ee32bf8b9e
lovkeshbogra <50145999+lovkeshbogra@users.noreply.github.com>
2023-01-16 06:01:30 -0800

    Update GCPFW_Vulnerable.tf

--- GCPFW_Vulnerable.tf
+++ GCPFW_Vulnerable.tf
@@ -8,7 +8,6 @@
   direction      = "INGRESS"
   source_ranges = ["0.0.0.0/0"]
   priority       = "1001"
-  target_tags    = "${var.tags}"
   allow { #1
   protocol = "tcp" #1
   ports = ["80","8080","15021","443","15017","15014","15018","8676","2022","8443","6443","53","22","3389"] #1
