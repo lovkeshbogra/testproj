03a025078e8eeda5ffeef9932e7144de982af365
lovkeshbogra <50145999+lovkeshbogra@users.noreply.github.com>
2023-01-16 05:57:01 -0800

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "google_compute_firewall" "firewall" {
+  project     = "hostproject-test"
+  name           = "test-rule-2"
+  network        = "projects/hostproject-test/global/networks/test"
+  provider       = "google-beta"
+  description    = "Allow Inbound"
+  #@enable_logging@
+  direction      = "INGRESS"
+  source_ranges = ["0.0.0.0/0"]
+  priority       = "1001"
+  target_tags    = "${var.tags}"
+  allow { #1
+  protocol = "tcp" #1
+  ports = ["80","8080","15021","443","15017","15014","15018","8676","2022","8443","6443","53","22","3389"] #1
+  } #1
