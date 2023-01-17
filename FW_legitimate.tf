18fcb88c4e1ad9313a3d6aec7cf74a544c432fff
lovkeshbogra <50145999+lovkeshbogra@users.noreply.github.com>
2023-01-16 23:45:35 -0800

    Update FW_legitimate.tf

--- FW_legitimate.tf
+++ FW_legitimate.tf
@@ -19,7 +19,7 @@ resource "compute_firewall" "firewall" {
   project     = "hostproject-test"
   name           = "test-rule-2"
   network        = "projects/hostproject-test/global/networks/test"
-  provider       = "google-AWS-AZURE"
+  provider       = "google-beta"
   description    = "Allow Inbound"
   #@enable_logging@
   direction      = "INGRESS"
@@ -36,7 +36,7 @@ resource "compute_firewall" "firewall" {
   project     = "hostproject-test"
   name           = "test-rule-1"
   network        = "projects/hostproject-test/global/networks/test"
-  provider       = "google-AWS-Azure"
+  provider       = "google-beta"
   description    = "Baseline Deny All"
   #@enable_logging@
   direction      = "EGRESS"
