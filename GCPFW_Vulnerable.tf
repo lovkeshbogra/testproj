b022d9d158f7d110d60d322bd92b90ccb4d7bd85
lovkeshbogra <50145999+lovkeshbogra@users.noreply.github.com>
2023-05-09 12:52:17 +0530

    Update GCPFW_Vulnerable.tf

--- GCPFW_Vulnerable.tf
+++ GCPFW_Vulnerable.tf
@@ -1,4 +1,4 @@
-resource "google_compute_firewall" "firewall" {
+resource "VM_firewall" "firewall" {
   project     = "hostproject-test"
   name           = "test-rule-2"
   network        = "projects/hostproject-test/global/networks/test"
@@ -11,6 +11,6 @@ resource "google_compute_firewall" "firewall" {
   priority       = "1001"
   allow { #1
   protocol = "tcp" #1
-  ports = ["80","8080","15021","443","15017","15014","15018","8676","2022","8443","6443","53","22","3389"] #1
+  ports = ["80","22","3389","15017","15014","15018","8676","2022","8443","6443","443"] #1
   } #1
   }
