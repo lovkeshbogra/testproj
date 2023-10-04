b31fd7c4d3d91b29db996e8da86009a25453d4bf
lovkeshbogra <50145999+lovkeshbogra@users.noreply.github.com>
2023-10-04 16:49:41 +0530

    Update AWSFW_Vulnerable.tf

--- AWSFW_Vulnerable.tf
+++ AWSFW_Vulnerable.tf
@@ -19,13 +19,7 @@ ingress {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
-ingress {
-    description      = "HTTP Allow"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+
 
 
 ingress {
