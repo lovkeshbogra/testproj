3097987fe8653dddf7e0b058be92ff480cee2cbd
lovkeshbogra <50145999+lovkeshbogra@users.noreply.github.com>
2024-12-10 11:06:46 +0530

    Update AWSFW_Vulnerable.tf

--- AWSFW_Vulnerable.tf
+++ AWSFW_Vulnerable.tf
@@ -15,7 +15,7 @@ data "aws_vpc" "GTM-AWS-EUS-INT-DFS-CS-VPC" {
 }
 
 resource "aws_security_group" "allow_HTTP_HTTPS_RDP" {
-  name        = "allow_HTTP_HTTPS_RDP1"
+  name        = "allow_HTTP_HTTPS_RDP10"
   description = "Allow HTTP, HTTPS, RDP inbound traffic"
   vpc_id      = data.aws_vpc.GTM-AWS-EUS-INT-DFS-CS-VPC.id
 
@@ -27,7 +27,13 @@ resource "aws_security_group" "allow_HTTP_HTTPS_RDP" {
     cidr_blocks = ["0.0.0.0/0"] # Open to all (not recommended for production)
   }
 
-
+ingress {
+    description      = "HTTP Allow"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]  # Unsecure (not recommended for production)
+  }
 
   ingress {
     description = "SSH Allow"
